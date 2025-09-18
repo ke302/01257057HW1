cb960a3874e25e51710bed2f3561064f4f1f8e73
chien <leoyu93.01.04@gmail.com>
2025-09-18 15:26:21 +0800

    Initial Commit

---
+++ 01257057HW1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01257057HW1
+//
+//  Created by user05 on 2025/9/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01257057HW1/_1257057HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  _1257057HW1App.swift
+//  01257057HW1
+//
+//  Created by user05 on 2025/9/18.
+//
+
+import SwiftUI
+
+@main
+struct _1257057HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
